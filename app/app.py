d5257261d9388e5c850994ef7563e12d6895a48e
vthatte2 <96803797+vthatte2@users.noreply.github.com>
2022-01-25 19:57:57 -0600

    Update app.py

--- app/app.py
+++ app/app.py
@@ -117,50 +117,6 @@ def crop_prediction():
 # render fertilizer recommendation result page
 
 
-@ app.route('/fertilizer-predict', methods=['POST'])
-def fert_recommend():
-    title = 'Harvestify - Fertilizer Suggestion'
-
-    crop_name = str(request.form['cropname'])
-    N = int(request.form['nitrogen'])
-    P = int(request.form['phosphorous'])
-    K = int(request.form['pottasium'])
-    # ph = float(request.form['ph'])
-
-    df = pd.read_csv('Data/fertilizer.csv')
-
-    nr = df[df['Crop'] == crop_name]['N'].iloc[0]
-    pr = df[df['Crop'] == crop_name]['P'].iloc[0]
-    kr = df[df['Crop'] == crop_name]['K'].iloc[0]
-
-    n = nr - N
-    p = pr - P
-    k = kr - K
-    temp = {abs(n): "N", abs(p): "P", abs(k): "K"}
-    max_value = temp[max(temp.keys())]
-    if max_value == "N":
-        if n < 0:
-            key = 'NHigh'
-        else:
-            key = "Nlow"
-    elif max_value == "P":
-        if p < 0:
-            key = 'PHigh'
-        else:
-            key = "Plow"
-    else:
-        if k < 0:
-            key = 'KHigh'
-        else:
-            key = "Klow"
-
-    response = Markup(str(fertilizer_dic[key]))
-
-    return render_template('fertilizer-result.html', recommendation=response, title=title)
-
-# render disease prediction result page
-
-
 
 # ===============================================================================================
 if __name__ == '__main__':
